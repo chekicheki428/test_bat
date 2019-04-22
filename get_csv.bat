6fc12403ce62646a8b4d5fc00e6b2f0c4804f639
chekicheki428 <chekicheki428@hotmail.com>
2019-04-23 00:58:55 +0900

    Update get_csv.bat

--- get_csv.bat
+++ get_csv.bat
@@ -1,5 +1,13 @@
 set tablename=%1
+set col_arg=%2
+set getcsv_sqlfile=%tablename%.sql
+set conn=
 
-echo sqlplus userid/pass %tablename%.sql
+echo spool %tablename%.csv       >%getcsv_sqlfile%
+echo select                     >>%getcsv_sqlfile%
+call getcolumns.bat             >>%getcsv_sqlfile%
+from %tablename%                >>%getcsv_sqlfile%
+echo where col_1 = '%col_arg%' ;>>%getcsv_sqlfile%
+echo quit                       >>%getcsv_sqlfile%
 
-type nul > %tablename%.csv
+sqlplus -s %conn% @%getcsv_sqlfile%
